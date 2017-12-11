5608b0eee50f1d39a452676b56745b81625715d6
Maryussuperboy <martsyalis@gmail.com>
2017-12-11 10:24:41 -0800

    write app, header and routes

---
+++ src/app/Error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default ({ error }) => {
+  if(!error) return null;
+  return (
+    <div className="error">
+      {Array.isArray(error) 
+        ? <ul>error.map(err => <li>err</li>)</ul>
+        : error.error ? error.error : error
+      }
+    </div>
+  );
+};
+
---
+++ src/app/Header.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+
+class Header extends PureComponent {
+  
+  render() {
+    return (
+      <header>
+        <h1>iTravel</h1>
+        <nav>
+          <span><NavLink to="/home">Home</NavLink></span>&nbsp;
+          <span><NavLink to="/search">Search</NavLink></span>&nbsp;
+          <span><NavLink to="/upload">Upload</NavLink></span>&nbsp;
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default connect(
+  null,
+  null
+)(Header);
