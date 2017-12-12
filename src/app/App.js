6185636895d4db53740dc33cda7be4ef35dc5aa6
Maryussuperboy <martsyalis@gmail.com>
2017-12-11 18:12:53 -0800

    rewriting for private Routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,46 +1,31 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { connect } from 'react-redux';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
-import Header from './Header'; 
-import Error from './Error';
-import Loading from './Loading';
-import Home from '../home/Home';
-import Search from '../search/Search';
-import UploadForm from '../upload/UploadForm';
 import { checkForToken } from '../auth/actions';
+import Routes from './Routes';
+import Header from './Header';
+import Loading from './Loading';
+import Error from './Error';
 
-class App extends PureComponent {
-
+class App extends Component {
   componentDidMount() {
     this.props.checkForToken();
   }
-  
-  render() {
-    const { loading, error, checkedToken } = this.props;
 
+  render() {
+    const { checkedToken, loading, error } = this.props;
     return (
       <Router>
-        { checkedToken && 
-        <div className="App">
-          <Header/>
-          <main>
-            <Switch>
-              <Route exact path="/" component = {Home}/>
-              {/* <Route exact path="/search" component = {Search}/>
-              <Route exact path="/upload" component = {UploadForm}/> */}
-              <Redirect to="/"/>
-            </Switch>
-            <Loading loading={loading}/>
-            <Error error={error}/>
-          </main>
-        </div>
+        { checkedToken &&
+          <div className="App">
+            <Header/>
+            <main>
+              <Routes/>
+            </main>
+          </div>
         }
+        {loading && <Loading/>}
+        { error && <Error/>}
       </Router>
     );
   }
@@ -48,7 +33,6 @@ class App extends PureComponent {
 
 export default connect(
   state => ({ 
-    user: state.auth.user,
     checkedToken: state.auth.checkedToken,
     loading: state.loading,
     error: state.error
