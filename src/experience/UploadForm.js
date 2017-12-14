3d197b4abf8928f4aa2599d63bbdd4229f830b90
Maryussuperboy <martsyalis@gmail.com>
2017-12-13 16:00:49 -0800

    add tags to upload and display tags

--- src/experience/UploadForm.js
+++ src/experience/UploadForm.js
@@ -16,8 +16,10 @@ export class UploadForm extends PureComponent {
       title: elements.title.value,
       description: elements.description.value,
       location: elements.location.value,
-      user: this.props.user._id
+      user: this.props.user._id,
+      tags: elements.tags.value.split(' ')
     };
+    console.log(exp.tags);
     this.props.addExperience(exp);
     this.setState({ redirect: true });
   }
@@ -30,6 +32,7 @@ export class UploadForm extends PureComponent {
             <input name="title" placeholder="title"/>
             <input name="description" placeholder="description"/>
             <input name="location" placeholder="location"/>
+            <input name="tags" placeholder="tags"/>
             <button type="submit">Add</button>
           </form> 
           { this.state.redirect && (<Redirect to="/MyExperiences"/>) }
